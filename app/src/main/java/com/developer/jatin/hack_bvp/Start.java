9cd7979a273c929a435c85fdc98b0fa9a096a0e3
Jatin Bindra <jatinbindra171998@gmail.com>
2017-10-10 17:46:40 +0530

    dev

--- app/src/main/java/com/developer/jatin/hack_bvp/Start.java
+++ app/src/main/java/com/developer/jatin/hack_bvp/Start.java
@@ -1,7 +1,9 @@
 package com.developer.jatin.hack_bvp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Start extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Proposals");
     }
 }
